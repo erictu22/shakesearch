197068ccc0176ce6e7a6b649dcf7a0dd698a905c
erictu22 <erictu222@gmail.com>
2023-04-08 13:40:38 -0400

    Search: Adds a search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchBar from './components/Searchbar';
 import { H1 } from './components/Text';
 
 
 function App() {
   return (
     <AppWrapper>
-      <H1> Shakesearch </H1>
-      
+      <MainContent>
+        <H1>Shakesearch</H1>
+        <SearchBar/>
+      </MainContent>
     </AppWrapper>
   );
 }
+
+const MainContent = styled.div`
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const AppWrapper = styled.div`
   background-color: rgba(52,53,65,1);
---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event : any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <SearchContainer>
+      <SearchInput
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <SearchButton>Search</SearchButton>
+    </SearchContainer>
+  );
+};
+
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  position: relative;
+`;
+
+const SearchInput = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  width: 100%;
+`;
+
+const SearchButton = styled.div`
+  border: none;
+  border-radius: 0 8px 8px 0;
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+  outline: none;
+  position: absolute;
+  right: 4px;
+  color: grey;
+`;
+
+export default SearchBar;
